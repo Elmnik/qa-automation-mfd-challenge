65d665366732d6b3b22f36ce8c003f90804a4a0b
Elmer Aguilar <33378750+Elmnik@users.noreply.github.com>
2025-09-06 15:59:57 -0600

    Update getObject.test.ts

--- backend/tests/getObject.test.ts
+++ backend/tests/getObject.test.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { ObjectsApi } from '../api/objectsApi';
 
-test('Test_001 - Get a single object', async () => {
+test('Test_001 - Get a single object', async () => { //Simple call using the main method
   const api = new ObjectsApi();
   const response = await api.getObject('7');
 
-  expect(response.status).toBe(200);
+  expect(response.status).toBe(200); //Basic Response from the api
 
   const data = await response.json();
   expect(data.id).toBe('7');
